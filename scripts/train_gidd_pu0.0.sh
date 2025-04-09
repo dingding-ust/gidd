740c3f4098fcf97c9a00eef88fef000fd1912543
Ding Ding <ddingab@connect.ust.hk>
2025-04-09 18:09:38 +0800

    update batch_size

--- scripts/train_gidd_pu0.0.sh
+++ scripts/train_gidd_pu0.0.sh
@@ -26,8 +26,20 @@ export TRANSFORMERS_CACHE=/scratch/PI/makchen/ddingab/datasets/huggingface/trans
 export TORCH_HOME=/scratch/PI/makchen/ddingab/datasets/torch
 export PYTHONPATH=$PYTHONPATH:/home/ddingab/gidd
 
+
+
+# 设置CUDA内存分配
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
+
 # 3. 确保切换到项目根目录
 cd /home/ddingab/gidd
 
-# 4. 运行命令: p_u=0.0
-torchrun --nnodes 1 --nproc_per_node 4 gidd/train.py --config-name gidd logging.run_name="'small-gidd+-owt-pu=0.0'" hydra.run.dir="/scratch/PI/makchen/ddingab/gidd/outputs/${now:%Y-%m-%d}/${now:%H-%M-%S}"
+# 使用较小的批次和序列长度
+torchrun --nnodes 1 --nproc_per_node 8 gidd/train.py --config-name gidd \
+  logging.run_name="'small-gidd+-owt-pu=0.0-reduced'" \
+  training.train_batch_size=8 \
+  training.eval_batch_size=8 \
+  training.gradient_accumulation_steps=8 \
+  data.max_seq_length=512 \
+  training.compile_model=False \
+  hydra.run.dir="/scratch/PI/makchen/ddingab/gidd/outputs/${now:%Y-%m-%d}/${now:%H-%M-%S}"
